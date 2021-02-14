54fd71219f897eab1e0faa5df607eaf104f43ce0
Kilian Agboton <kilian.agboton@gmail.com>
2021-02-13 23:05:12 -0500

    project controller refactoring

--- ppmtapi/src/main/java/io/kagboton/ppmt/web/ProjectController.java
+++ ppmtapi/src/main/java/io/kagboton/ppmt/web/ProjectController.java
@@ -1,6 +1,7 @@
 package io.kagboton.ppmt.web;
 
 import io.kagboton.ppmt.domain.Project;
+import io.kagboton.ppmt.services.ObjectValidationService;
 import io.kagboton.ppmt.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,26 +25,17 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private ObjectValidationService objectValidationService;
+
 
     // create a new project
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
         // handle and return errors from the request
-        if (result.hasErrors()) {
-
-            // retrieve all errors from the current request
-            List<FieldError> errors = result.getFieldErrors();
-
-            // extract useful information from the list and build a new list
-            Map<String, String> errorMap = new HashMap<>();
-            for(FieldError error : errors){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            // return error fields with their related error message
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = objectValidationService.objectResultValidation(result);
+        if (errorMap != null) return  errorMap;
 
         // persist the project if everything is ok
         Project savedProject = projectService.saveOrUpdate(project);
